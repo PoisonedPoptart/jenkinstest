d9735380c7d8708d4c944f6f7b519865870a4347
bigdata <bigdata@node82.bigdata.com>
2016-01-20 09:39:28 +0800

    new version

--- jenkins/helloworld/src/oata/HelloWorld.java
+++ jenkins/helloworld/src/oata/HelloWorld.java
@@ -1,6 +1,6 @@
 package oata;  
 public class HelloWorld {     
 	public static void main(String[] args) {         
-		System.out.println("Hello World, new version");     
+		System.out.println("Hello World, new version v22");     
 	}
 } 
